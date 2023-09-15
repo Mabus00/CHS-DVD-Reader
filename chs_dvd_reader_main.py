307323ef76da87af5668a18f4e1608ec274616e8
Mabus00 <wayne.renaud@gmail.com>
2023-09-15 14:25:39 -0300

    added delete current_database if it exists

--- chs_dvd_reader_main.py
+++ chs_dvd_reader_main.py
@@ -81,6 +81,10 @@ class CHSDVDReaderApp(QMainWindow):
         utils.close_database(self.ui.createDatabaseTextBrowser, self.master_database_conn)
 
     def run_checker(self):
+         # delete if necessary then build new database
+        if os.path.exists(self.master_database_name):
+            utils.delete_existing_database(self.current_database_name, self.ui.runCheckerTextBrowser)
+
         # establish database connections; operate under assumption that master_database won't be created each time widget is used
         self.master_database_conn, self.master_database_cursor = utils.get_database_connection(self.master_database_name, self.ui.createDatabaseTextBrowser)
         self.current_database_conn, self.current_database_cursor = utils.get_database_connection(self.current_database_name, self.ui.runCheckerTextBrowser)
@@ -91,8 +95,8 @@ class CHSDVDReaderApp(QMainWindow):
 
         # create current database
         # instantiate generate_database and pass instance of database_signals to create the current month's database
-        #self.create_db = CreateDatabase(self.run_checker_signals, self.ui.checker_data_input_path.text(), self.current_database_conn, self.current_database_cursor, self.ui.runCheckerTextBrowser)
-        #self.create_db.generate_database()
+        self.create_db = CreateDatabase(self.run_checker_signals, self.ui.checker_data_input_path.text(), self.current_database_conn, self.current_database_cursor, self.ui.runCheckerTextBrowser)
+        self.create_db.generate_database()
 
         # instantiate run_checker and pass instance of database_signals, etc...
         self.run_checker = RunChecker(self.run_checker_signals, self.master_database_name, self.master_database_conn, self.master_database_cursor, self.current_database_name, self.current_database_conn, self.current_database_cursor, self.ui.runCheckerTextBrowser)
--- run_checker.py
+++ run_checker.py
@@ -21,28 +21,34 @@ class RunChecker():
 
     # Method to compare databases
     def compare_databases(self):
-        # Get the current year and month in the format "YYYYMM"
-        # current_year_month = datetime.now().strftime('%Y%m')
-
-        # Initialize a list to store matching foldernames (code commented out)
-        #matching_foldernames = []
-
-        ''' STEPS '''
-
         # Call the compare_tables method to perform table comparison
         result = self.compare_tables()
         print(result)
+    
+        # Additional steps for the comparison process (commented out)
+        # 3. start with master and find the same table (with the newer date) in current
+        # 4. for each row compare: chart number, Edn Date, Last NM, Ed number and Title and report any discrepancies/ store them in a local table. Chart numbers matching will be a challenge if not the same.
+        # 5. report all discrepancies (for now) as errors so you can see what the differences are.
+        # 6. as you move forward and you can differentiate between new charts, new editions, and withdrawn charts, add these details to each specific local table, with the remainder staying in errors.
+        # 7. add the ability to print the result as a pdf.
+        # 8. once all has been verified and the user is happy, overwrite the master with the current.
+
+        # Print a message to indicate that the checker has run
+        print('run_checker')
+
         # Compare the DTG of the master database and the current database; ensure the current is at least one month newer than the master; if more, confirm with the user
         # Get the table names from the master database
 
+    # Method to compare table yyyymmdd in the master and current databases to make sure they're the same within the db
     def compare_tables(self):
+        # Define table prefixes to be compared
         table_prefixes = ['EastDVD', 'WestDVD']
         
-        match_result, month_result = False, False
-
         # Initialize dictionaries to store dates found in each database
         master_dates = {}
         current_dates = {}
+
+        match_result, month_result = False, False
 
         # Loop through the table prefixes and compare dates
         for prefix in table_prefixes:
@@ -59,24 +65,25 @@ class RunChecker():
         # Compare the EastDVD and WestDVD tables separately for both master and current databases
         if (
             master_dates.get('EastDVD') == master_dates.get('WestDVD') and
-            current_dates.get('EastDVD') == current_dates.get('WestDVD') and
-            datetime.strptime(current_dates.get('EastDVD'), '%Y%m%d').month > datetime.strptime(master_dates.get('EastDVD'), '%Y%m%d').month
-            ):
-                match_result, month_result = True, True
+            current_dates.get('EastDVD') == current_dates.get('WestDVD')
+        ):
+            utils.update_text_browser(self.text_browser_widget, "\nEast & West DVD dates match within both databases.")
+            match_result = True
+        else:
+            utils.update_text_browser(self.text_browser_widget, "\nEast & West DVD dates do not match within both databases.")
+
+        # Compare the EastDVD tables separately for both master and current databases
+        master_date = datetime.strptime(master_dates.get('EastDVD'), '%Y%m%d')
+        current_date = datetime.strptime(current_dates.get('EastDVD'), '%Y%m%d')
+
+        if current_date.month > master_date.month:
+            utils.update_text_browser(self.text_browser_widget, "\nCurrent East & West DVD dates are a month or more later than the Master Database dates.")
+            month_result = True
+        else:
+            utils.update_text_browser(self.text_browser_widget, "\nCurrent East & West DVD dates are not a month or more later than the Master Database dates.")
 
         return match_result, month_result
 
-        
-        # Additional steps for the comparison process (commented out)
-        # 3. start with master and find the same table (with the newer date) in current
-        # 4. for each row compare: chart number, Edn Date, Last NM, Ed number and Title and report any discrepancies/ store them in a local table. Chart numbers matching will be a challenge if not the same.
-        # 5. report all discrepancies (for now) as errors so you can see what the differences are.
-        # 6. as you move forward and you can differentiate between new charts, new editions, and withdrawn charts, add these details to each specific local table, with the remainder staying in errors.
-        # 7. add the ability to print the result as a pdf.
-        # 8. once all has been verified and the user is happy, overwrite the master with the current.
-
-        # Print a message to indicate that the checker has run
-        print('run_checker')
 
 # Main execution block (can be used for testing)
 if __name__ == "__main__":
