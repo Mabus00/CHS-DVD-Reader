76a17090153852eadb04d2b8e59c04a5c1255356
Mabus00 <wayne.renaud@gmail.com>
2023-12-19 11:06:21 -0400

    continued dev of toc and hyperlinks

--- create_pdf_report.py
+++ create_pdf_report.py
@@ -5,12 +5,8 @@ from reportlab.platypus.frames import Frame
 from reportlab.platypus.tableofcontents import TableOfContents
 from reportlab.lib.styles import ParagraphStyle as PS
 from reportlab.lib import colors
-from reportlab.lib.units import cm, inch
-from reportlab.pdfgen import canvas
-from PyPDF2 import PdfReader, PdfWriter, Transformation
+from reportlab.lib.units import cm
 from hashlib import sha1
-import os
-import io
 
 class PDFReport(BaseDocTemplate):
     def __init__(self, path, **kw):
@@ -22,24 +18,27 @@ class PDFReport(BaseDocTemplate):
         # set template for document
         template = PageTemplate('normal', [Frame(1.5*cm, 1.5*cm, 15*cm, 25*cm, id='F1')], onPageEnd = self.footer)
         self.addPageTemplates(template)
-
         # set styles for document
         self.styles = [
             PS(fontSize=20, name='Title', spaceAfter=30, leading=12),
             PS(fontSize=14, name='Normal', spaceAfter=10, leading=12),
         ]
-
-        self.toc = TableOfContents()
-
         # set styles for toc
         self.toc = TableOfContents()
         self.toc.levelStyles = [
             PS(fontName='Times-Bold', fontSize=20, name='TOCHeading1', spaceBefore=10, leading=16),
-            PS(fontSize=18, name='TOCHeading2', spaceBefore=5, leading=12),
+            PS(fontSize=12, name='TOCHeading2', spaceBefore=5, leading=12, leftIndent=20),
         ]
-
-        # append toc to document
-        #self.elements.append(self.toc)
+        # set style for tables
+        self.table_style = TableStyle([
+            ('BACKGROUND', (0, 0), (-1, 0), colors.gray),  # First row shading
+            ('BACKGROUND', (0, 1), (-1, 1), colors.lightgrey),       # Second row shading
+            ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
+            ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black),
+            ('WORDWRAP', (2, 0), (2, -1), 1),  # Enable word wrap for the third column
+            ('SPAN', (0, 0), (-1, 0)),  # Merge cells for the first row
+        ])
 
     def afterFlowable(self, flowable):
         print("""Registers TOC entries.""")
@@ -90,21 +89,21 @@ class PDFReport(BaseDocTemplate):
         self.elements.append(Spacer(1, 12))  # Add space after the paragraph
 
     def add_table(self, content):
-
         table_data = []
-
         # Separate content into folders
         folders = [list(filter(None, folder.split('\n'))) for folder in content.split('\n\n') if folder.strip()]
-
-        # Add the message before the table
+        # Extract message and print before the table
         message = folders[0][0]
         self.add_paragraph(message)
+        # first line in all folders contains the message and the first folder title; now that message has been used move the folder title into the message position
+        # only needs to be done once for all folders
         folders[0] = folders[0][1:]
-
+        # process folder content
         for folder in folders:
             # Table title (Folder Name)
             folder_title = folder[0]
-            #self.elements.append(folder_title) # Example: Adding a table entry in TOC
+            self.doHeading(folder_title, self.toc.levelStyles[1])
+            self.elements.append(Spacer(1, 12))  # Add space after each table
             # Commence construction of table for the folder
             table_data = [[folder_title]]  # Include folder_title in the merged top row
             if len(folder) > 1 and '\t' in folder[1]: # if there's more rows and the rows have columns; as opposed to single row messages (if more rows)
@@ -114,26 +113,12 @@ class PDFReport(BaseDocTemplate):
                 folder_table_data = [column_headers]  # Add the column headers
                 folder_table_data.extend([row.split('\t') for row in folder[2:]])  # Finally, add the data rows
                 table_data.extend(folder_table_data)  # Extend with the data
-
-            table = Table(table_data, hAlign='LEFT', repeatRows=2)
-
-            table_style = TableStyle([
-                ('BACKGROUND', (0, 0), (-1, 0), colors.gray),  # First row shading
-                ('BACKGROUND', (0, 1), (-1, 1), colors.lightgrey),       # Second row shading
-                ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
-                ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
-                ('GRID', (0, 0), (-1, -1), 1, colors.black),
-                ('WORDWRAP', (2, 0), (2, -1), 1),  # Enable word wrap for the third column
-                ('SPAN', (0, 0), (-1, 0)),  # Merge cells for the first row
-            ])
-
-            table.setStyle(table_style)
-            
-            self.elements.append(table)
-            self.elements.append(Spacer(1, 12))  # Add space after each table
-
-        # if len(self.elements) > 0:  # Check if there are any elements added
-        #     self.elements.append(PageBreak())
+                table = Table(table_data, hAlign='LEFT', repeatRows=2)
+                table.setStyle(self.table_style)            
+                self.elements.append(table)
+                self.elements.append(Spacer(1, 12))  # Add space after each table
+            else:
+                self.add_paragraph("\n".join(folder))  # Treat single line as a paragraph
 
     def save_report(self):
         pdf_report = PDFReport(self.path, pagesize=letter)
